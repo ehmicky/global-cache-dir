3e8463ed55e9df32efda85a1279b3a8b6ef5c039
ehmicky <ehmicky@gmail.com>
2019-08-09 10:00:00 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import getCacheDir from 'cachedir'
+
+// Get the global cache directory
+const globalCacheDir = async function() {
+  const cacheDir = getCacheDir()
+  return cacheDir
+}
+
+// We do not use `export default` because Babel transpiles it in a way that
+// requires CommonJS users to `require(...).default` instead of `require(...)`.
+module.exports = globalCacheDir
