7ed09284a0f2c7a49dd3acfa23269efaeabdd179
ehmicky <ehmicky@gmail.com>
2022-07-20 20:11:13 +0200

    Improve tsconfig.json

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,5 +1,6 @@
-import globalCacheDir from 'global-cache-dir'
 import { expectError, expectType } from 'tsd'
+
+import globalCacheDir from './main.js'
 
 expectType<string>(await globalCacheDir('name'))
 expectError(await globalCacheDir())
